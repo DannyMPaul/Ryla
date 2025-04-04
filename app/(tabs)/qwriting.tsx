83a6486f8876a9fce2e48534c6b353f1ddea7bec
Joeson Stanes <joesonstanes1360@gmail.com>
2025-04-04 06:57:10 +0530

    dubugs

--- app/(tabs)/qwriting.tsx
+++ app/(tabs)/qwriting.tsx
@@ -338,7 +338,7 @@ const WritingAssessmentScreen = () => {
 
           <TouchableOpacity
             style={styles.continueButton}
-            onPress={() => router.replace("./Frn/welcome")}
+            onPress={() => router.replace("./Welcome")}
           >
             <Text style={styles.continueButtonText}>Submit</Text>
           </TouchableOpacity>
